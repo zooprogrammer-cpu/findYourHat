dc2cbf0b481eafdb265bdd183cd7d88355e19708
Ash B <ashbasn@gmail.com>
2024-12-11 22:18:11 -0600

    Printing from the class

--- main.js
+++ main.js
@@ -4,3 +4,16 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+  constructor(input) {
+    this.input = input;
+  }
+
+  print() {
+    console.log('Printing this');
+  }
+}
+
+const newField = new Field('Hello');
+newField.print();
